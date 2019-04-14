adc925a6258101bd6331ad677d9392fbd85b6372
Jayaraj8905 <jayaraj8905@gmail.com>
2019-04-14 09:09:18 +0800

    Creater Header Section for opening page

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Card, CardItem } from 'native-base';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,49 +22,74 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Container>
-        <Header>
+        <Header style={styles.header}>
           <Left>
             <Button transparent>
               <Icon name='menu' />
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title>Friendz Ride</Title>
           </Body>
           <Right />
         </Header>
-        <Content>
-          <Text>
-            This is Content Section
-          </Text>
+        <Content style={styles.content}>
+          <View style={styles.headerextend}/>
+          <View style={styles.slope}/>
+          <Card style={styles.card}>
+            <CardItem>
+              <Body>
+                <Text>
+                  Your text here
+                  Your text here
+                  Your text here
+                  Your text here
+                  Your text here
+                  Your text here
+                  Your text here
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
         </Content>
-        <Footer>
+        {/* <Footer>
           <FooterTab>
             <Button full>
               <Text>Footer</Text>
             </Button>
           </FooterTab>
-        </Footer>
+        </Footer> */}
       </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  header: {
+    height: 75,
+    backgroundColor: '#835FFD',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  headerextend: {
+    height: 20,
+    backgroundColor: '#835FFD',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
+  slope: {
+    overflow: 'hidden',
+    borderStyle: 'solid',
+    borderLeftWidth: 125,
+    borderTopWidth: 20,
+    borderLeftColor: 'transparent',
+    borderTopColor: '#835FFD',
+    borderRightWidth: 125,
+    borderRightColor: 'transparent'
+  },
+  content: {
+    backgroundColor: '#EBF0F4',
+  },
+  card: {
+    marginTop: -50,
     marginBottom: 5,
-  },
+    marginLeft: 40,
+    marginRight: 40,
+  }
 });
