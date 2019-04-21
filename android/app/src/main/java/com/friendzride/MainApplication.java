4056c9589cbe197d43683f6ea167a7aaed8aa925
Jayaraj8905 <jayaraj8905@gmail.com>
2019-04-21 19:49:16 +0800

    FB Login Integration
    References:
    The main Reference is
    https://developers.facebook.com/docs/react-native/getting-started
    
    Other References
    https://github.com/facebook/react-native-fbsdk
    https://medium.com/react-native-training/facebook-sdk-with-login-for-react-native-apps-in-2018-196601804f66
    
    The hash key is not able to generated. So copied the hash key after we
    tried the login

--- android/app/src/main/java/com/friendzride/MainApplication.java
+++ android/app/src/main/java/com/friendzride/MainApplication.java
@@ -9,11 +9,16 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+            new FBSDKPackage(mCallbackManager),
             new VectorIconsPackage()
       );
     }
@@ -41,6 +46,9 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   @Override
   public void onCreate() {
     super.onCreate();
